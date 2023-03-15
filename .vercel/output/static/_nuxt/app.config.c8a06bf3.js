9c9b57486eb7c9015f0b823e284f13808e057c4b
BeanTTL <teethat.lsk@gmail.com>
2023-03-15 18:24:31 +0700

    nitro preset

---
+++ .vercel/output/static/_nuxt/app.config.c8a06bf3.js
@@ -0,0 +1,1 @@
+import{K as u,P as s}from"./entry.2b1abe07.js";function a(e,n){return u()._useHead(e,n)}const o={};s(o);export{a as u};
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -8,4 +8,7 @@ export default defineNuxtConfig({
       autoprefixer: {},
     },
   },
+  nitro: {
+    preset: "vercel"
+  }
 });
